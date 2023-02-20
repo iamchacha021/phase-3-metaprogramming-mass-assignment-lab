19c9d0341a2b4b7db74e6ada6e5d1b88906e708d
iamchacha021 <iamchacha021@gmail.com>
2023-02-20 16:33:43 +0300

    done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,17 @@
+require 'pry'
 class Person
   # your code here
+  def initialize(args)
+    args.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
+
+bob_attributes = { name: "Bob", hair_color: "Brown" }
+bob = Person.new(bob_attributes)
+
+susan_attributes = { name: "Susan", height: "5'11\"", eye_color: "Green" }
+susan = Person.new(susan_attributes)
+binding.pry
